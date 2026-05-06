38072e4030c064499c817b9b08442fa8675aec43
Sravya <your_github_email@example.com>
2026-05-06 13:38:11 +0530

    fix: use standard PrismaClient and run migrations on start

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,5 @@
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = prisma;
